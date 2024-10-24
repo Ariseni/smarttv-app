3ec3708056fadc9d065975bce61bf15e35d09064
Ivo Ljubicic <ivo.ljubicic@hotmail.com>
2024-10-24 14:41:24 +0200

    fix arrow UI

--- src/components/Slider/Arrows.tsx
+++ src/components/Slider/Arrows.tsx
@@ -6,21 +6,21 @@ type ArrowProps = {
 
 export const CustomPrevArrow = (props: ArrowProps) => (
   <div
-    className="absolute -left-[5px] z-10 flex h-full w-40 cursor-pointer items-center justify-center p-2 hover:bg-[linear-gradient(to_left,_rgba(0,0,0,0.3),_rgba(0,0,0,0.8))]"
+    className="group absolute -left-[5px] z-10 flex h-full w-40 cursor-pointer items-center justify-center p-2 hover:bg-[linear-gradient(to_left,_rgba(0,0,0,0.3),_rgba(0,0,0,0.8))]"
     style={{ top: "50%", transform: "translateY(-50%)" }}
     onClick={props.onClick}
   >
-    <ArrowLeft color="white" size={60} />
+    <ArrowLeft color="white" size={60} className="hidden group-hover:block" />
   </div>
 );
 
 // Custom right arrow
 export const CustomNextArrow = (props: ArrowProps) => (
   <div
-    className="absolute -right-[5px] z-10 flex h-full w-40 cursor-pointer items-center justify-center p-2 hover:bg-[linear-gradient(to_right,_rgba(0,0,0,0.3),_rgba(0,0,0,0.8))]"
+    className="group absolute -right-[5px] z-10 flex h-full w-40 cursor-pointer items-center justify-center p-2 hover:bg-[linear-gradient(to_right,_rgba(0,0,0,0.3),_rgba(0,0,0,0.8))]"
     style={{ top: "50%", transform: "translateY(-50%)" }}
     onClick={props.onClick}
   >
-    <ArrowRight color="white" size={60} />
+    <ArrowRight color="white" size={60} className="hidden group-hover:block" />
   </div>
 );
