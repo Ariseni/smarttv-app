2b8456b698a180d632dfb34b8c03df6db4726cce
Ivo Ljubicic <ivo.ljubicic@hotmail.com>
2024-10-25 15:40:13 +0200

    uncomment Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,11 @@
 import { NavMenu } from "./NavMenu";
 import { Search } from "./Search";
 
-export default function Header () {
+export function Header() {
   return (
     <header className="fixed top-0 z-50 flex h-20 w-full items-center justify-between bg-gray-800 p-0 text-white">
       <NavMenu />
       <Search />
     </header>
   );
-};
-
+}
